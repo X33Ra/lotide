7aa64ab4e548158f5181ac92a81186079ae97a0e
MrChan <mrkcwayne@gmail.com>
2023-05-26 14:15:40 -0600

    changed overall code to compliment each function it now responds to where the letters are

--- test.js
+++ test.js
@@ -1,24 +1,3 @@
-const letterPositions = function(sentence) {
-  const results = {};
-
-  for (let i = 0; i < sentence.length; i++) {
-    const char = sentence[i];
-    if (char !== ' ') {
-      if (!results[char]) {
-        results[char] = [i];
-      } else {
-        results[char].push(i);
-      }
-    }
-  }
-
-  return results;
-};
-
-const banana = letterPositions('hello');
-console.log(banana);
-
-
 const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length) {
     return false;
@@ -41,5 +20,33 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
- 
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    const char = sentence[i];
+    if (char !== ' ') {
+      if (!results[char]) {
+        results[char] = [i];
+      } else {
+        results[char].push(i);
+      }
+    }
+  }
+
+  return results;
+};
+
+const banana = letterPositions('hello');
+console.log(banana);
+
+const testPositions = {
+  'h': [0],
+  'e': [1],
+  'l': [2, 3],
+  'o': [4]
+};
+
+for (const char in testPositions) {
+  assertArraysEqual(banana[char], testPositions[char]);
+}

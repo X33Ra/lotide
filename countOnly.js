45cbc92e42dc42e81fc9156818f31abd7cf5029e
MrChan <mrkcwayne@gmail.com>
2023-05-25 19:43:34 -0600

    more conditions

--- countOnly.js
+++ countOnly.js
@@ -7,7 +7,8 @@ const assertEqual = function(actual, expected) {
 };
 
 
-
+// VVVVVVV
+// The function should report back how many instances of each string were found in the allItems array of strings.
 
 const countOnly = function(allItems, itemsToCount) {
 // allItems: an array of strings that we need to look through

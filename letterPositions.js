ae41f33adbd0e7957d01af53965f052ebb9b8fb8
MrChan <mrkcwayne@gmail.com>
2023-05-26 12:53:02 -0600

    Implement a function called letterPositions.

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,21 @@
+// return all the indices (zero-based positions) in the string where each character is found.
+// For each letter, instead of returning just one number to represent its number of occurrences, multiple numbers may be needed to represent all the places in the string that it shows up.
+
+
 const letterPositions = function(sentence) {
   const results = {};
   // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    const char = sentence[i];
+    if (char !== ' ') {
+      if (!results[char]) {
+        results[char] = [i];
+      } else {
+        results[char].push(i);
+      }
+    }
+  }
+
   return results;
 };
+console.log(letterPositions);

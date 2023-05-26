a967108d1ace50bb4f2283997ed1474f23e250f0
MrChan <mrkcwayne@gmail.com>
2023-05-25 21:01:23 -0600

    wrote some conditions

---
+++ countLetters.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// create a function called countLetters
+// function should take in a sentence (as a string)
+// and then return a count of each of the letters in that sentence
+
+
+
+assertEqual(1, 1);

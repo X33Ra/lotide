36c200ef694d941a9223b63add11460bc959a76e
MrChan <mrkcwayne@gmail.com>
2023-05-26 14:21:38 -0600

    testing out

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
 const eqArrays = function(actual, expected) {
-  // Compare the lengths of the arrays
   if (actual.length !== expected.length) {
     return false;
   }
 
-  // Compare each element of the arrays
   for (let i = 0; i < actual.length; i++) {
     if (actual[i] !== expected[i]) {
       return false;
@@ -15,7 +13,6 @@ const eqArrays = function(actual, expected) {
 };
 
 const assertArraysEqual = function(actual, expected) {
-  // Use eqArrays to compare the arrays
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
   } else {
@@ -26,11 +23,9 @@ const assertArraysEqual = function(actual, expected) {
 const letterPositions = function(sentence) {
   const results = {};
 
-  // Iterate through the characters in the sentence
   for (let i = 0; i < sentence.length; i++) {
     const char = sentence[i];
     if (char !== ' ') {
-      // Update the results object with the positions of each character
       if (!results[char]) {
         results[char] = [i];
       } else {
@@ -52,7 +47,6 @@ const testPositions = {
   'o': [4]
 };
 
-// Compare the positions of each character using assertArraysEqual
 for (const char in testPositions) {
   assertArraysEqual(banana[char], testPositions[char]);
 }

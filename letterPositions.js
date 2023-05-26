d299a30fd3c660914c9a1bb79c97a86219c7dcf6
MrChan <mrkcwayne@gmail.com>
2023-05-26 14:51:46 -0600

    fixed the problem of my function not relating with other functions

--- letterPositions.js
+++ letterPositions.js
@@ -1,28 +1,3 @@
-// return all the indices (zero-based positions) in the string where each character is found.
-// For each letter, instead of returning just one number to represent its number of occurrences, multiple numbers may be needed to represent all the places in the string that it shows up.
-
-
-const letterPositions = function(sentence) {
-  const results = {};
-  // logic to update results here
-  for (let i = 0; i < sentence.length; i++) {
-    const char = sentence[i];
-    if (char !== ' ') {
-      if (!results[char]) {
-        results[char] = [i];
-      } else {
-        results[char].push(i);
-      }
-    }
-  }
-
-  return results;
-};
-const banana = letterPositions('hello');
-console.log(banana);
-
-
-//copy eqArrays function below
 const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length) {
     return false;
@@ -37,8 +12,6 @@ const eqArrays = function(actual, expected) {
   return true;
 };
 
-// Copy assertArrayEqual function below
-
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
@@ -47,5 +20,33 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-// Added a test case to define assetArrayEqual function
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // Pass
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    const char = sentence[i];
+    if (char !== ' ') {
+      if (!results[char]) {
+        results[char] = [i];
+      } else {
+        results[char].push(i);
+      }
+    }
+  }
+
+  return results;
+};
+
+const banana = letterPositions('hello');
+console.log(banana);
+
+const testPositions = {
+  'h': [0],
+  'e': [1],
+  'l': [2, 3],
+  'o': [4]
+};
+
+for (const char in testPositions) {
+  assertArraysEqual(banana[char], testPositions[char]);
+}

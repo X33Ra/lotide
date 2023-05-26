f036dc0f854d5cba2c182663cb9edc07579d8eda
MrChan <mrkcwayne@gmail.com>
2023-05-25 20:10:13 -0600

    do they pass or not?

--- countOnly.js
+++ countOnly.js
@@ -12,9 +12,34 @@ const assertEqual = function(actual, expected) {
 
 const countOnly = function(allItems, itemsToCount) {
 // allItems: an array of strings that we need to look through
+  console.log(countOnly);
 
 
+  // itemsToCount: an object specifying what to count
+  const results = {};
+  
+  // ... perform the necessary computations to populate the results object
+  
+  return results;
 
-// itemsToCount: an object specifying what to count
 
-}
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);

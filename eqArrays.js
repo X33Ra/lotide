962b42d92a1f25ad1332ced43e92c0a5c969ceb3
MrChan <mrkcwayne@gmail.com>
2023-05-26 13:44:49 -0600

    console logged the test results

--- eqArrays.js
+++ eqArrays.js
@@ -14,8 +14,8 @@ const eqArrays = function(actual, expected) {
   return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false

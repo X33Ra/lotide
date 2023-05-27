9ed0e569d8b5dd0d66905bcbfe1d5d7d87aa16df
MrChan <mrkcwayne@gmail.com>
2023-05-26 19:20:11 -0600

    Added a function findKeyByValue to get the value of the key to report wheter it's undefined or a key

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -14,6 +14,17 @@ const assertEqual = function(actual, expected) {
 // should scan the object and return the first key which contains the given value
 // If no key with that given value is found, then it should return undefined.
 // Use assertEqual to write test cases for various scenarios.
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object.hasOwnProperty.call(object,key)) {
+      if (object[key] === value) {
+        return key;
+      }
+    }
+  }
+  return undefined;
+}
+
 
 // Here are some test assertions for you to implmement and make pass
 

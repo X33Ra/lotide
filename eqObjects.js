c057a23d70ee87e4069a00fa532649779fde8cc0
MrChan <mrkcwayne@gmail.com>
2023-05-30 16:48:17 -0600

    Step 1 take 2 options and decide if it's true or false based on perfect match

--- eqObjects.js
+++ eqObjects.js
@@ -10,3 +10,8 @@ const assertEqual = function(actual, expected) {
 
 // Conditions:
 // Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+};

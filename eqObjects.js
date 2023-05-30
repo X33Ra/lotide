9427c350f0153a5a94ad1250a9dee71848044dc9
MrChan <mrkcwayne@gmail.com>
2023-05-30 17:13:33 -0600

    added a test case plus the assert true or false defined answer

--- eqObjects.js
+++ eqObjects.js
@@ -21,12 +21,22 @@ const eqObjects = function(object1, object2) {
   if (keys1.length !== keys2.length) {
     return false;
   }
+
+  for (const key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
-for (const key of keys1) {
-  if (object1[key] !== object2[key]) {
-    return false;
-  }
-}
+// Test cases
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 2, a: 1 };
+const obj3 = { a: 1, b: 2, c: 3 };
 
-return true;
+assertEqual(eqObjects(obj1, obj2), true); // Objects should be equal
+assertEqual(eqObjects(obj1, obj3), false); // Objects have different lengths
+
+

20024415a734c2521cc77930fb3ac9947b002fe7
MrChan <mrkcwayne@gmail.com>
2023-05-30 17:18:36 -0600

    did some minor fix on syntax

--- eqObjects.js
+++ eqObjects.js
@@ -41,11 +41,11 @@ assertEqual(eqObjects(obj1, obj3), false); // Objects have different lengths
 
 
 const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
+const anotherShirtObject = { size: "medium", color: "red" };
 eqObjects(shirtObject , anotherShirtObject); // => true
 //We need to use that return value in combination with assertEquals to test if the function is working correctly.
-assertEquals(eqObjects(shirtObject , anotherShirtObject), true);
+assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
 
-const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 eqObjects(shirtObject , longSleeveShirtObject); // => false
-assertEquals(eqObjects(shirtObject , longSleeveShirtObject), false);
+assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);

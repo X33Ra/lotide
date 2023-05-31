c52bbb41e82d3addd2be76f9314649f901a072e1
MrChan <mrkcwayne@gmail.com>
2023-05-30 18:28:26 -0600

    copy eqobject's function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,27 @@
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    if (typeof value1 === 'object' && typeof value2 === 'object') {
+      if (!eqObjects(value1, value2)) {
+        return false;
+      }
+    } else if (Array.isArray(value1) && Array.isArray(value2)) {
+      if (!eqArrays(value1, value2)) {
+        return false;
+      }
+    } else if (value1 !== value2) {
+      return false;
+    }
+  }
+
+  return true;
+};

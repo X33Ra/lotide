1b6e79ba9f1755397f0ac5ddc41d13ea7d835124
MrChan <mrkcwayne@gmail.com>
2023-05-31 19:59:53 -0600

    added some minor frame rules

---
+++ map.js
@@ -0,0 +1,6 @@
+// Our map function will take in two arguments:
+
+// An array to map
+// A callback function
+// The map function will return a new array based on the results of the callback function.
+

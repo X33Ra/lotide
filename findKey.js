43136d0e5464331429c2467e3ba7beddb0ffe20c
MrChan <mrkcwayne@gmail.com>
2023-06-01 22:16:01 -0600

    added test keys to prove it actually works

--- findKey.js
+++ findKey.js
@@ -27,3 +27,19 @@ const findKey = function(object, callback) {
   // if no key found return undefined [check]
   return undefined;
 };
+
+const obj1 = {
+  "blue": { stars: 2 },
+  "green": { stars: 1 },
+  "yellow": { stars: 3 },
+  "red": { stars: 2 }
+};
+
+const result1 = findKey(obj1, value => value.stars === 1);
+assertEqual(result1, "green");
+
+const result2 = findKey(obj1, value => value.stars === 4);
+assertEqual(result2, undefined);
+
+const result3 = findKey(obj1, value => value.stars > 2);
+assertEqual(result3, "yellow");

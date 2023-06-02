4d2b9451f3d5ba0864ae4b4d13edd0a1f5333848
MrChan <mrkcwayne@gmail.com>
2023-06-01 22:11:39 -0600

    added rules for code guideline

--- findKey.js
+++ findKey.js
@@ -6,3 +6,10 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+// rules:
+// Implement the function findKey which takes in an object and a callback.
+// It should scan the object and return the first key for which the callback returns a truthy value
+// If no key is found, then it should return undefined.
+// Use assertEqual to write test cases for various scenarios.
+// The tests should be written below the definition of your function.

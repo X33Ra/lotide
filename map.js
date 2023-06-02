65edf496e19a3a6e4cacf7e1fa4dcf664a5ec967
MrChan <mrkcwayne@gmail.com>
2023-06-01 20:00:13 -0600

    changed my loop to [for of] loop instead of [for loop]

--- map.js
+++ map.js
@@ -3,9 +3,18 @@
 // An array to map
 // A callback function
 // The map function will return a new array based on the results of the callback function.
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
 
 const words = ["ground", "control", "to", "major", "tom"];
 
 // Instruction:
 // Call map and pass in our words and a simple arrow function.
 
+const results1 = map(words, word => word[0]);
+console.log(results1);
